dca87affbf001fec5138810bfe36598a82af5946
FenXD <d.193121@gmail.com>
2019-02-05 20:07:01 +0300

    Добавлен javaDoc

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,51 @@
 package ru.job4j.calculator;
 
+/**
+ * Простейший калкуляток
+ */
 public class Calculator {
     private double result;
 
+    /**
+     * Сложение
+     * @param first - первое слагаемое
+     * @param second - творое слагаемое
+     */
     public void add(double first, double second) {
         this.result = first + second;
     }
 
+    /**
+     * Вычитание
+     * @param first - уменьшаемое
+     * @param second - вычитаемое
+     */
     public void substract(double first, double second) {
         this.result = first - second;
     }
 
+    /**
+     * Деление
+     * @param first - делимое
+     * @param second - делитель
+     */
     public void divide(double first, double second) {
         this.result = first / second;
     }
 
+    /**
+     * Умножение
+     * @param first - превый множитель
+     * @param second - второй множитель
+     */
     public void multiply(double first, double second) {
         this.result = first * second;
     }
 
+    /**
+     * Получение результата действия
+     * @return результат
+     */
     public double getResult() {
         return this.result;
     }
