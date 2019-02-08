9843a78e71d67db314fb1a9189380612ad4bc402
FenXD <d.193121@gmail.com>
2019-02-08 14:46:37 +0300

    Поправка

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -15,6 +15,6 @@ public class Max {
     }
 
     public int max(int first, int second, int third) {
-        return max(max(first, second),third);
+        return max(max(first, second), third);
         }
 }
