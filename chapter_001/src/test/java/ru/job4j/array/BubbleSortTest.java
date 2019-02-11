63896d2bb852c6fb16aa33af232551ec4deb334e
FenXD <d.193121@gmail.com>
2019-02-11 19:05:26 +0300

    -

--- chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/BubbleSortTest.java
@@ -14,7 +14,7 @@ public class BubbleSortTest {
     @Test
     public void when1782394650Then0123456789() {
         BubbleSort sort = new BubbleSort();
-        int[] array = new int[] {1, 7, 8, 2, 3, 9, 4, 6, 5, 0};
+        int[] array = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
         int[] result = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         assertThat(sort.sort(array), is(result));
 
