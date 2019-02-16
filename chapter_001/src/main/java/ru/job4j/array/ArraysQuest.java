db2faa3098981130ecf843dfb58ff344d71e7354
FenXD <d.193121@gmail.com>
2019-02-16 14:10:14 +0300

    1

--- chapter_001/src/main/java/ru/job4j/array/ArraysQuest.java
+++ chapter_001/src/main/java/ru/job4j/array/ArraysQuest.java
@@ -5,20 +5,24 @@ public class ArraysQuest {
         int firstIndex = 0;
         int secondIndex = 0;
         int[] third = new int[first.length + second.length];
-        for (int i = 0; i < third.length; i++) {
+        for (int i = 0; i < third.length - 1; i++) {
             if (first[firstIndex] < second[secondIndex]) {
                 third[i] = first[firstIndex];
-                if (firstIndex < first.length - 1) {
                     firstIndex++;
-                }
             } else {
                 third[i] = second[secondIndex];
-                if (secondIndex < second.length - 1) {
-                    secondIndex++;
-                }
-
+                secondIndex++;
             }
+
+
+        }
+        if (firstIndex != first.length - 1) {
+            third[third.length - 1] = second[secondIndex];
+        }
+        if (secondIndex != second.length - 1) {
+            third[third.length - 1] = first[firstIndex];
         }
         return third;
     }
 }
+
--- chapter_001/src/test/java/ru/job4j/array/ArraysQuestTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArraysQuestTest.java
@@ -8,9 +8,9 @@ public class ArraysQuestTest {
     @Test
     public void when01And23Then0123() {
         ArraysQuest array = new ArraysQuest();
-        int[] first = new int[] {0, 1, 4, 5, 7};
-        int[] second = new int[] {2, 3, 6};
-        int[] result = new int[] {0, 1, 2, 3, 4, 5, 6, 7};
+        int[] first = new int[] {0, 1, 4, 5, 6, 9};
+        int[] second = new int[] {2, 3, 7, 8};
+        int[] result = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         assertThat(array.thirdArray(first, second), is(result));
     }
 }
