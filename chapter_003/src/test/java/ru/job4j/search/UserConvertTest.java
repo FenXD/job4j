9b79f8b2e35d1c8be8b648195e3a4682976fe0d4
FenXD <d.193121@gmail.com>
2020-02-07 14:58:44 +0300

    Организовать сортировку User[#94863]

--- chapter_003/src/test/java/ru/job4j/search/UserConvertTest.java
+++ chapter_003/src/test/java/ru/job4j/search/UserConvertTest.java
@@ -13,10 +13,9 @@ public class UserConvertTest {
     @Test
     public void whenPut2UsersThenGetHashMap() {
         UserConvert converter = new UserConvert();
-        List<User> users = new ArrayList<User>();
-        users.add(new User(1, "Max", "Msk"));
-        users.add(new User(2, "Ben", "Spb"));
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "Max", "Msk", 21));
+        users.add(new User(2, "Ben", "Spb", 22));
         assertThat(converter.convertUser(users).get(1).getName(), is("Max"));
-
     }
 }
