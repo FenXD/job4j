19bd434211bbd3e71b9f21e5f34454e6fadf93bc
FenXD <d.193121@gmail.com>
2020-02-26 15:13:34 +0300

    fix

--- chapter_003/src/main/java/ru/job4j/search/StringCompare.java
+++ chapter_003/src/main/java/ru/job4j/search/StringCompare.java
@@ -25,12 +25,9 @@ public class StringCompare implements Comparator<String> {
     public int compare(String s1, String s2) {
         int result = 0;
         int i = 0;
-        int maxLength = s1.length() > s2.length() ? s1.length() : s2.length();
         int minLength = s1.length() > s2.length() ? s2.length() : s1.length();
-        while (i < maxLength) {
-            if (i < minLength && i < maxLength) {
+        while (i < minLength) {
                 result += Character.compare(s1.charAt(i), s2.charAt(i));
-            }
             if (result != 0) {
                 return result;
             }
